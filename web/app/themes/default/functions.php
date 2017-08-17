8badeab872b18cc3f3898619fa151f70809e0afa
Kristian Lauttamus <kristian.lauttamus@phz.fi>
2017-08-17 16:03:30 +0300

    theme changes

--- web/app/themes/default/functions.php
+++ web/app/themes/default/functions.php
@@ -1,12 +1,12 @@
 <?php
 
-function enqueue_script() {
+function enqueue_scripts() {
     wp_enqueue_script('app', get_template_directory_uri() . '/style.css');
 }
 
-function enqueue_style() {
+function enqueue_styles() {
     wp_enqueue_style('style', get_stylesheet_uri() . '/app.js');
 }
 
-add_action( 'wp_enqueue_scripts', 'enqueue_script' );
-add_action( 'wp_enqueue_scripts', 'enqueue_style' );
+add_action( 'wp_enqueue_scripts', 'enqueue_scripts' );
+add_action( 'wp_enqueue_scripts', 'enqueue_styles' );
