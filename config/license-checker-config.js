90c9e97691db6ea7d55b576875e80362b15fd248
Kristian Lauttamus <kristian.lauttamus@phz.fi>
2017-08-28 15:15:45 +0300

    adds license-checker to the project

---
+++ config/license-checker-config.js
@@ -0,0 +1,29 @@
+module.exports = {                     
+ "allowedPackages": [],
+ "disallowedPackages": [],
+ "allowedLicenses": [
+   "MIT",
+   "Apache-2.0",
+   "ISC",
+   "Apache",
+   "WTF",
+   "Public Domain",
+   "MPL",
+   "CC-BY-3.0",
+   "CC-BY-4.0",
+   "BSD-3-Clause",
+   "BSD-2-Clause",
+   "X11",
+   "WTFPL",
+   "CDDL",
+   "LGPL",
+   "Apache 2.0",
+   "MIT/X11",
+   "AFL",
+   "(MIT AND CC-BY-3.0)",
+   "Unlicense"
+ ],
+ strictMode: true,
+ ignoreDevDependencies: true,
+ warnOnUnknown: true
+}
